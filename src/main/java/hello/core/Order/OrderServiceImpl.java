2c3922aa4b87f9c0f1e619755e00244618533a31
rudqls007 <rudqls007@gmail.com>
2023-05-22 21:03:56 +0900

    OCP (Open/Closed Principle) 개방-폐쇄 원칙
    5가지 원칙 중 가장 중요한 원칙이다.
    
    소프트웨어 요소는 확장에는 열려있으나 변경에는 닫혀있어야 한다.
    
    엥? 뭔 말이지 싶을 것이다. 확장을 하려면 당연히 기존 코드를 변경해야 하는 것이 아닌가?
    
    이는 다형성을 활용하면 개방-폐쇄의 원칙을 잘 지킬 수가 있다.
    
    잘 생각해보면 다른 하나를 만드는 것 자체는 기존 코드를 전혀 변경하지 않는 것이다.
    
    인터페이스를 구현(implement)한 새로운 클래스를 하나 만들어서 새로운 기능을 구현하더라도, 클라이언트에서는 코드를 변경하지 않아도 된다.
    
    (클라이언트 코드의 객체를 생성(new)하는 부분에서는 코드를 변경해야 할 수도 있다. 하지만 스프링에서는 스프링 컨테이너와 DI(Dependency Injection) 등으로 OCP를 완전히 지킬 수 있다.)
    
    DIP (Dependency Inversion Principle) 의존관계 역전 원칙
    
    OCP 원칙과 함께 DIP도 정말 중요한 원칙이다.
    
    쉽게 말해, 클라이언트 코드가 구현 클래스에 의존하지 말고, 인터페이스에 의존하라는 뜻이다.
    
    (구현체에 의존하지 말고 역할에 의존해야 한다.)
    
    의존성 주입(Dependency Injection)은 "프로그래머는 추상화에 의존해야지, 구체화에 의존하면 안된다."라는 원칙을 따르는 방법 중 하나이다.
    
    (클라이언트 코드의 객체를 생성(new)하는 부분에서는 구현 클래스에 의존해야 한다. 하지만 스프링에서는 스프링 컨테이너와 DI(Dependency Injection) 등으로 DIP를 완전히 지킬 수 있다.)

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,8 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
+    // 구체(구현체 - 클래스)에 의존하지 않고 인터페이스에만 의존하도록 DIP를 지킴.
+    private  DiscountPolicy discountPolicy;
     @Override
     public Order createorder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
