4fa21f9e7e00b6723485ed2d592dfc7efe0a63a9
rudqls007 <rudqls007@gmail.com>
2023-05-23 16:41:24 +0900

    Appconfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,12 +11,19 @@ import hello.core.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService OrderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy(){
+        return  new FixDiscountPolicy();
+    }
 
 }
