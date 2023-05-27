cdcc6742d41fe4e775370d141621fc5dce3fb1e3
rudqls007 <rudqls007@gmail.com>
2023-05-27 15:16:23 +0900

    SPRING - 컴포넌트 스캔

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -6,13 +6,17 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     // 구체(구현체 - 클래스)에 의존하지 않고 인터페이스에만 의존하도록 DIP를 지킴.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
---
+++ src/test/java/hello/core/sacn/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.sacn.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
